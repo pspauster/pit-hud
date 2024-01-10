0aa5cf3c87051b277143237f5034c54592864a68
Spauster <ps4375@ad.law.nyu.edu>
2024-01-10 15:48:51 -0500

    initial commit

---
+++ nyc.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(janitor)
+library(readxlsb)
+
+
+years <- as.character(2014:2023)
+  
+nyc <- map_dfr(years, ~read_xlsb("2007-2023-PIT-Counts-by-CoC.xlsb", sheet = .x, col_types = "string")%>%
+                 clean_names() %>% 
+                 filter(co_c_number == "NY-600")%>%
+                 mutate(year = .x)
+               )
+
+nyc_clean <- nyc %>% 
+  select(-co_c_category) %>% 
+  mutate_at(vars(-(1:3)), ~as.numeric(str_replace_all(.x, ",", "")))
