20ded3d53a16b95fd840e8ac74e36df54c1c2a26
Spauster <ps4375@ad.law.nyu.edu>
2024-01-17 15:38:38 -0500

    added summary from excel macro

--- nyc.R
+++ nyc.R
@@ -13,4 +13,13 @@ nyc <- map_dfr(years, ~read_xlsb("2007-2023-PIT-Counts-by-CoC.xlsb", sheet = .x,
 
 nyc_clean <- nyc %>% 
   select(-co_c_category) %>% 
-  mutate_at(vars(-(1:3)), ~as.numeric(str_replace_all(.x, ",", "")))
+  mutate_at(vars(-(1:3)), ~as.numeric(str_replace_all(.x, ",", ""))) %>% 
+  pivot_longer(starts_with(c("overall_", "sheltered", "unsheltered")),
+               names_to = c("type"),
+               values_to = "value")
+
+
+nyc_summary <- read_csv("individuals_nyc_summary.csv")
+
+
+
